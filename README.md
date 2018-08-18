6146525ab5ce9a9fe64778f84f8f92e560d97aae
yokv <ez4tzl@hotmail.com>
2018-08-18 19:41:51 +0800

    update

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # msa-spring-simple
 spring cloud 微服务最简版本，用于测试部署swarm Kubernetes
 
-##项目说明
+## 项目说明
 A通过eureka注册中心调用B（定时任务）
--- app-b/src/main/java/yokv/FooController.java
+++ app-b/src/main/java/yokv/FooController.java
@@ -7,15 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 
-/**
- * CREATE BY Yo ON 2018/1/26 20:26
- */
 @RestController
 public class FooController {
     private static final Logger LOGGER = LoggerFactory.getLogger(FooController.class);
 
     @GetMapping("/foo")
-    public String getApplicationName() {
+    public String foo() {
         String value = String.valueOf(LocalDateTime.now());
         LOGGER.info("APP-B: {}", value);
         return value;
