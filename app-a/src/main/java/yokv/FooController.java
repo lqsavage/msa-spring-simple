cf50e0e402919afe30ecf48f6b7689b70737a95c
yo <ez4tzl@hotmail.com>
2018-11-15 22:04:39 +0800

    调用

--- app-a/src/main/java/yokv/FooController.java
+++ app-a/src/main/java/yokv/FooController.java
@@ -1,7 +1,9 @@
 package yokv;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -9,9 +11,13 @@ import java.time.LocalDateTime;
 
 @RestController
 public class FooController {
+    @Autowired
+    private RestTemplate restTemplate;
+
     @GetMapping("/foo")
     public String foo() throws UnknownHostException {
-        return "  APP-B:  " + InetAddress.getLocalHost().getHostAddress() + LocalDateTime.now();
+        String value = restTemplate.getForEntity("http://APP-B/foo", String.class).getBody();
+        return "  APP-A:  " + LocalDateTime.now() + InetAddress.getLocalHost().getHostAddress() + value;
     }
 }
 
